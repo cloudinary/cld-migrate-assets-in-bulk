aa69ec59444f4afd3705ed739797e58aed936a08
achumachenko-cloudinary <andriy.chumachenko@cloudinary.com>
2025-04-10 11:33:47 -0700

    Using mock smd config. Updated tests to reflect adjustments made to adopted implementation.

--- __plugins/cld-structured-metadata-mapper.test.js
+++ __plugins/cld-structured-metadata-mapper.test.js
@@ -1,6 +1,110 @@
 'use strict';
 
-const metadata_mapper = require('./CloudinaryMetadataMapper').plugin;
+
+const MOCK_SMD_STRUCTURE = [
+    {
+    type: 'set',
+    external_id: 'smd_msl',
+    label: 'SMD MSL',
+    mandatory: false,
+    default_value: null,
+    validation: null,
+    default_disabled: false,
+    restrictions: { readonly_ui: false },
+    datasource: {
+        values: [
+        {
+            external_id: 'msl_option_a',
+            value: 'MSL Option A',
+            state: 'active'
+        },
+        {
+            external_id: 'msl_option_b',
+            value: 'MSL Option B',
+            state: 'active'
+        },
+        {
+            external_id: 'msl_option_c',
+            value: 'MSL Option C',
+            state: 'active'
+        }
+        ]
+    },
+    lazy_datasource_update: false,
+    allow_dynamic_list_values: false
+    },
+    {
+    type: 'enum',
+    external_id: 'smd_ssl',
+    label: 'SMD SSL',
+    mandatory: false,
+    default_value: null,
+    validation: null,
+    default_disabled: false,
+    restrictions: { readonly_ui: false },
+    datasource: {
+        values: [
+        {
+            external_id: 'ssl_option_a',
+            value: 'SSL Option A',
+            state: 'active'
+        },
+        {
+            external_id: 'ssl_option_b',
+            value: 'SSL Option B',
+            state: 'active'
+        },
+        {
+            external_id: 'ssl_option_c',
+            value: 'SSL Option C',
+            state: 'active'
+        }
+        ]
+    },
+    lazy_datasource_update: false,
+    allow_dynamic_list_values: false
+    },
+    {
+    type: 'date',
+    external_id: 'smd_date',
+    label: 'SMD Date',
+    mandatory: false,
+    default_value: null,
+    validation: null,
+    default_disabled: false,
+    restrictions: { readonly_ui: false }
+    },
+    {
+    type: 'integer',
+    external_id: 'smd_number',
+    label: 'SMD Number',
+    mandatory: false,
+    default_value: null,
+    validation: null,
+    default_disabled: false,
+    restrictions: { readonly_ui: false }
+    },
+    {
+    type: 'string',
+    external_id: 'smd_text',
+    label: 'SMD Text',
+    mandatory: false,
+    default_value: null,
+    validation: null,
+    default_disabled: false,
+    restrictions: { readonly_ui: false }
+    }
+]
+
+const metadata_mapper = require('./cld-structured-metadata-mapper').plugin;
+
+jest.spyOn(metadata_mapper, 'init').mockImplementation(async () => {
+    await new Promise(resolve => setTimeout(resolve, 100));
+    // Mocking the private #metadata_structure class property
+    Object.defineProperty(metadata_mapper, 'metadata_structure', {
+        get: () => MOCK_SMD_STRUCTURE
+    });
+});
 
 beforeAll(async () => {
     await metadata_mapper.init();
@@ -10,19 +114,19 @@ describe("Passing in metadata", () => {
     it('should update a Enum value', async () => {
         const uploadOptions = {};
         const inputFields = {
-            "Show hidden": "No"
+            "SMD SSL": "SSL Option B"
         }
 
         metadata_mapper.process(uploadOptions,inputFields);
 
-        expect(uploadOptions.metadata.show_hidden).toEqual('no');
+        expect(uploadOptions.metadata.smd_ssl).toEqual('ssl_option_b');
         expect(Object.keys(inputFields).length).toEqual(0);
     });
 
     it('should update Set values', async () => {
         const uploadOptions = {};
         const inputFields = {
-            "Color shades": "Amber, Gold",
+            "SMD MSL": "MSL Option C, MSL Option A",
         }
 
         metadata_mapper.process(uploadOptions,inputFields);
@@ -30,22 +134,22 @@ describe("Passing in metadata", () => {
         console.log(uploadOptions);
         console.log(inputFields);
 
-        expect(uploadOptions.metadata.color_shades).toEqual(['amber', 'gold']);
+        expect(uploadOptions.metadata.smd_msl).toEqual(['msl_option_c', 'msl_option_a']);
         expect(Object.keys(inputFields).length).toEqual(0);
     });
 
     it('should ignore an unknown value', async () => {
         const uploadOptions = {};
         const inputFields = {
-            "Show hidden": "No",
-            "doesnt_exist": "This"
+            "SMD SSL": "SSL Option C",
+            "Does Not Exist": "This"
         }
 
         metadata_mapper.process(uploadOptions,inputFields);
 
-        expect(uploadOptions.metadata.show_hidden).toEqual('no');
+        expect(uploadOptions.metadata.smd_ssl).toEqual('ssl_option_c');
         expect(Object.keys(inputFields).length).toEqual(1);
-        expect(inputFields.doesnt_exist).toEqual('This');
+        expect(inputFields['Does Not Exist']).toEqual('This');
     });
 
     it('should ignore empty values', async () => {
